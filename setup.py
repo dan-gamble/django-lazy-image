9dfd9c30a8ef1a86d2d92ddaf1fe222d22a1dd43
Dan Gamble <dan@dangamble.co.uk>
2017-11-16 17:06:14 +0000

    Release 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 def readme():
     """print long description"""
